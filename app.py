11ede0823803a72084fca624ce3c24507ae9e64a
matt2g <matt.gc3@gmail.com>
2021-06-04 15:43:13 -0600

    LCU API connector

---
+++ app.py
@@ -0,0 +1,22 @@
+from lcu_driver import Connector
+
+connector = Connector()
+
+
+@connector.ready
+async def connect(connection):
+    print('LCU API is ready to be used.')
+
+
+@connector.ws.register('/lol-summoner/v1/current-summoner', event_types=('UPDATE',))
+async def icon_changed(connection, event):
+    print(f'The summoner {event.data["displayName"]} was updated.')
+
+
+connector.start()
+
+
+@connector.close
+def disconnect(connection):
+    print('The client was closed')
+    await connector.stop()

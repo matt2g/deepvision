f799639ed59916a4c22b16f2a770a5511616c194
matt2g <matt.gc3@gmail.com>
2021-06-05 22:17:13 -0600

    LCU API connector

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from lcu_driver import Connector
 
-connector = Connector()
+
+def get_lockfile():
+    import os
+    path = os.path.join('/home/matt/Games/league-of-legends/drive_c/Riot Games/League of Legends/', 'lockfile')
+    if os.path.isfile(path):
+        file = open(path, 'r')
+        text = file.readline()
+        file.close()
+        return text
+    return None
+
+
+connector = Connector(get_lockfile())
 
 
 @connector.ready
@@ -8,15 +20,15 @@ async def connect(connection):
     print('LCU API is ready to be used.')
 
 
-@connector.ws.register('/lol-summoner/v1/current-summoner', event_types=('UPDATE',))
+@connector.ws.register('/lol-summoner/v1/summoners/', event_types=('UPDATE',))
 async def icon_changed(connection, event):
     print(f'The summoner {event.data["displayName"]} was updated.')
 
 
-connector.start()
-
-
 @connector.close
-def disconnect(connection):
+async def disconnect(connection):
     print('The client was closed')
     await connector.stop()
+
+
+connector.start()

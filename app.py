3df280f15b048831d07bc5f1ce266ecbaedaaba1
matt2g <matt.gc3@gmail.com>
2021-06-15 17:18:26 -0600

    LCU API connector

--- app.py
+++ app.py
@@ -18,35 +18,36 @@ def get_lockfile():
     return None
 
 
-if get_lockfile() is None:
-    connector = Connector()
-else:
-    connector = Connector(str=get_lockfile())
-game = Game()
+def app():
+    if get_lockfile() is None:
+        connector = Connector()
+    else:
+        connector = Connector(str=get_lockfile())
+    game = Game()
+
+    @connector.ready
+    async def connect(connection):
+        print('LCU connection ready')
+
+    @connector.ws.register('/lol-champ-select/v1/session', event_types=('CREATE',))
+    async def create_game(connection, event):
+        await game.create_game(connection)
+        game.get_op_gg_info()
+
+        @connector.ws.register('/lol-champ-select/v1/session', event_types=('UPDATE',))
+        async def updated_rune_page(connection2, event2):
+            runes = game.get_runes(event2.data)
+            if runes is not None:
+                perks, primaryStyleId, subStyleId = runes
+                await game.update_perks(connection2, perks, 'deepvision', primaryStyleId, subStyleId)
+
+    @connector.close
+    async def disconnect(connection):
+        print('The client was closed')
+        await connector.stop()
+
+    connector.start()
 
 
-@connector.ready
-async def connect(connection):
-    print('LCU connection ready')
-
-
-@connector.ws.register('/lol-champ-select/v1/session', event_types=('CREATE',))
-async def create_game(connection, event):
-    await game.create_game(connection)
-    game.get_op_gg_info()
-
-    @connector.ws.register('/lol-champ-select/v1/session', event_types=('UPDATE',))
-    async def updated_rune_page(connection, event2):
-        runes = game.get_runes(event2.data)
-        if runes is not None:
-            perks, primaryStyleId, subStyleId = runes
-            await game.update_perks(connection, perks, 'deepvision', primaryStyleId, subStyleId)
-
-
-@connector.close
-async def disconnect(connection):
-    print('The client was closed')
-    await connector.stop()
-
-
-connector.start()
+if __name__ == '__main__':
+    app()
